85da84b724fe72f7644f494f65e8f2448bc981fe
Andrew Loeliger <aloelige@cern.ch>
2018-11-04 01:43:10 +0100

    Cross section weighting script added

---
+++ NtuplePolishing/AddCrossSectionWeightings.py
@@ -0,0 +1,75 @@
+import ROOT
+import sys
+from tqdm import tqdm
+from array import array
+
+def AddCrossSectionWeightings(FileToRun):
+    
+    ReweightFile = ROOT.TFile(FileToRun,"UPDATE")
+    CrossSectionWeighting = array('f',[0])
+
+    ReweightFile.mt_tree.Branch('CrossSectionWeighting',CrossSectionWeighting,'CrossSectionWeighting/F')
+
+    #I believe this is the correct distribution to get?
+    TotalNumberOfEvents = ReweightFile.eventCount.GetBinContent(1)
+
+    for i in tqdm(range(ReweightFile.mt_tree.GetEntries())):
+        ReweightFile.mt_tree.GetEntry(i)
+        
+        LHCLumi = 41.370e15
+        if ("WW.root" in FileToRun):
+            CrossSectionWeighting = LHCLumi * 118.7e-12 / TotalNumberOfEvents
+        elif ("WZ.root" in FileToRun):
+            CrossSectionWeighting = LHCLumi * 47.13e-12 / TotalNumberOfEvents
+        elif("ZZ.root" in FileToRun):
+            CrossSectionWeighting = LHCLumi * 16.523e-12 / TotalNumberOfEvents
+        elif("W.root" in FileToRun
+             or "W1.root" in FileToRun
+             or "W2.root" in FileToRun
+             or "W3.root" in FileToRun
+             or "W4.root" in FileToRun):
+            if(ReweightFile.mt_tree.numGenJets==0):
+                CrossSectionWeighting = 110.1887
+            elif(ReweightFile.mt_tree.numGenJets==1):
+                CrossSectionWeighting = 14.1549
+            elif(ReweightFile.mt_tree.numGenJets==2):
+                CrossSectionWeighting = 7.43847
+            elif(ReweightFile.mt_tree.numGenJets==3):
+                CrossSectionWeighting = 2.40205
+            elif(ReweightFile.mt_tree.numGenJets==4):
+                CrossSectionWeighting = 2.14075
+        elif("TTTo2L2Nu.root" in FileToRun):
+            CrossSectionWeighting = LHCLumi * 88.34e-12 / TotalNumberOfEvents
+        elif("TTToHadronic.root" in FileToRun):
+            CrossSectionWeighting = LHCLumi * 377.96e-12 / TotalNumberOfEvents
+        elif("TTToSemiLeptonic.root" in  FileToRun):
+            CrossSectionWeighting = LHCLumi * 365.45e-12 / TotalNumberOfEvents
+        elif("DY.root" in FileToRun
+             or "DY1.root" in FileToRun
+             or "DY2.root" in FileToRun
+             or "DY3.root" in FileToRun
+             or "DY4.root" in FileToRun):
+            if(ReweightFile.mt_tree.numGenJets==0):
+                CrossSectionWeighting = 3.009
+            elif(ReweightFile.mt_tree.numGenJets==1):
+                CrossSectionWeighting = 0.589
+            elif(ReweightFile.mt_tree.numGenJets==2):
+                CrossSectionWeighting = 0.612
+            elif(ReweightFile.mt_tree.numGenJets==3):
+                CrossSectionWeighting = 0.767
+            elif(ReweightFile.mt_tree.numGenJets==4):
+                CrossSectionWeighting = 0.690
+        elif("Data.root" in  FileToRun):
+            CrossSectionWeighting = 1.0
+        else:
+            print("Unrecognized input sample! Defaulting to unweighted events!")
+        ReweightFile.mt_tree.Fill()
+
+    ReweightFile.mt_tree.Write()
+    ReweightFile.Write()
+    ReweightFile.Close()
+
+if __name__ == "__main__":
+    for File in sys.argv[1:]:
+        print("Processing the cross sections weights for "+File)
+        AddCrossSectionWeightings(File)
