1ae505a65bc89d470f32763ddca69ed7e52e31b4
Andrew Loeliger <aloelige@cern.ch>
2018-11-12 13:58:39 +0100

    Initial control plot code

---
+++ ControlPlotCode/RunControlPlotCode.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+root -b -q ControlPlotCreator.cc\(\"Data\"\)
+
+root -b -q ControlPlotCreator.cc\(\"DY\"\)
+root -b -q ControlPlotCreator.cc\(\"DY1\"\)
+root -b -q ControlPlotCreator.cc\(\"DY2\"\)
+root -b -q ControlPlotCreator.cc\(\"DY3\"\)
+root -b -q ControlPlotCreator.cc\(\"DY4\"\)
+
+root -b -q ControlPlotCreator.cc\(\"TTTo2L2Nu\"\)
+root -b -q ControlPlotCreator.cc\(\"TTToHadronic\"\)
+root -b -q ControlPlotCreator.cc\(\"TTToSemiLeptonic\"\)
+
+root -b -q ControlPlotCreator.cc\(\"W\"\)
+root -b -q ControlPlotCreator.cc\(\"W1\"\)
+root -b -q ControlPlotCreator.cc\(\"W2\"\)
+root -b -q ControlPlotCreator.cc\(\"W3\"\)
+root -b -q ControlPlotCreator.cc\(\"W4\"\)
+
+root -b -q ControlPlotCreator.cc\(\"WW\"\)
+root -b -q ControlPlotCreator.cc\(\"WZ\"\)
+root -b -q ControlPlotCreator.cc\(\"ZZ\"\)
