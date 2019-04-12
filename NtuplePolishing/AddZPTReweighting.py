c7eb746f7b14e07b7c4f14a2e44d44b99463d138
Andrew Loeliger <aloelige@cern.ch>
2019-04-12 22:50:33 +0200

    Pulled mu scale factors from the weights

--- NtuplePolishing/AddZPTReweighting.py
+++ NtuplePolishing/AddZPTReweighting.py
@@ -41,24 +41,52 @@ def ApplyZPTReweighting(File,args):
         ZPTWeighting[0]=1.0
         ZPTWeighting_DOWN[0] = 0.9 * ZPTWeighting[0]
         ZPTWeighting_UP[0] = 1.1 * ZPTWeighting[0]
-
-        Trigger24 = (ReweightFile.mt_Selected.passMu24 and ReweightFile.mt_Selected.matchMu24_1 
-                 and ReweightFile.mt_Selected.filterMu24_1 and ReweightFile.mt_Selected.pt_1 > 25.0)
-        Trigger27 = (ReweightFile.mt_Selected.passMu27 and ReweightFile.mt_Selected.matchMu27_1 
-                 and ReweightFile.mt_Selected.filterMu27_1 and ReweightFile.mt_Selected.pt_1 > 28.0)
-        Trigger2027 = (ReweightFile.mt_Selected.passMu20Tau27 and ReweightFile.mt_Selected.matchMu20Tau27_1 
-                   and ReweightFile.mt_Selected.filterMu20Tau27_1                    
-                   and ReweightFile.mt_Selected.filterMu20Tau27_2
-                   and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_2 > 31 
-                   and ReweightFile.mt_Selected.pt_1 < 25
-                   and abs(ReweightFile.mt_Selected.eta_2 < 2.1))
-    #no tau trigger matching in embedded
-        if(FileName == "Embedded.root"):
+        
+        if args.year == "2016":
+            raise RuntimeError("2016 not implemented! Implement me!")
+        elif args.year == "2017":
+            Trigger24 = (ReweightFile.mt_Selected.passMu24 and ReweightFile.mt_Selected.matchMu24_1 
+                         and ReweightFile.mt_Selected.filterMu24_1 and ReweightFile.mt_Selected.pt_1 > 25.0)
+            Trigger27 = (ReweightFile.mt_Selected.passMu27 and ReweightFile.mt_Selected.matchMu27_1 
+                         and ReweightFile.mt_Selected.filterMu27_1 and ReweightFile.mt_Selected.pt_1 > 28.0)
             Trigger2027 = (ReweightFile.mt_Selected.passMu20Tau27 and ReweightFile.mt_Selected.matchMu20Tau27_1 
-                           and ReweightFile.mt_Selected.filterMu20Tau27_1
+                           and ReweightFile.mt_Selected.filterMu20Tau27_1                    
+                           and ReweightFile.mt_Selected.filterMu20Tau27_2
                            and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_2 > 31 
                            and ReweightFile.mt_Selected.pt_1 < 25
                            and abs(ReweightFile.mt_Selected.eta_2 < 2.1))
+    #no tau trigger matching in embedded
+            if(FileName == "Embedded.root"):
+                Trigger2027 = (ReweightFile.mt_Selected.passMu20Tau27 and ReweightFile.mt_Selected.matchMu20Tau27_1 
+                               and ReweightFile.mt_Selected.filterMu20Tau27_1
+                               and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_2 > 31 
+                               and ReweightFile.mt_Selected.pt_1 < 25
+                               and abs(ReweightFile.mt_Selected.eta_2 < 2.1))
+        elif args.year == "2018":
+            Trigger24 = (ReweightFile.mt_Selected.passMu24 and ReweightFile.mt_Selected.matchMu24_1 
+                         and ReweightFile.mt_Selected.filterMu24_1 and ReweightFile.mt_Selected.pt_1 > 25.0)
+            Trigger27 = False
+            if(FileName == "Data.root"):
+                if (ReweightFile.mt_Selected.run >= 317509): #hps trigger, no filter
+                    Trigger2027 = (ReweightFile.mt_Selected.passMu20HPSTau27 
+                                   and ReweightFile.mt_Selected.matchMu20HPSTau27_1
+                                   and ReweightFile.mt_Selected.matchMu20HPSTau27_2
+                                   and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_1 < 25
+                                   and ReweightFile.mt_Selected.pt_2 > 28)
+                    if (ReweightFile.mt_Selected.run < 317509): #non hps trigger, can filter
+                        Trigger2027 = (ReweightFile.mt_Selected.passMu20Tau27 
+                                       and ReweightFile.mt_Selected.matchMu20Tau27_1
+                                       and ReweightFile.mt_Selected.matchMu20Tau27_2
+                                       and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_1 < 25
+                                       and ReweightFile.mt_Selected.pt_2 > 28
+                                       and ReweightFile.mt_Selected.filterMu20Tau27_1
+                                       and ReweightFile.mt_Selected.filterMu20Tau27_2)
+            else: #all hps cross trigger, ignore HPS filters
+                Trigger2027 = (ReweightFile.mt_Selected.passMu20HPSTau27 
+                               and ReweightFile.mt_Selected.matchMu20HPSTau27_1
+                               and ReweightFile.mt_Selected.matchMu20HPSTau27_2
+                               and ReweightFile.mt_Selected.pt_1 > 21 and ReweightFile.mt_Selected.pt_1 < 25
+                               and ReweightFile.mt_Selected.pt_2 > 28)
 
         if(FileName != "Embedded.root"
            and FileName != "Data.root"):
@@ -70,11 +98,7 @@ def ApplyZPTReweighting(File,args):
             ZPTWeighting[0] = ZPTWeighting[0]*SecondWorkSpace.function("m_id_kit_ratio").getVal()
             NominalWeight = SecondWorkSpace.function("zptmass_weight_nom").getVal()
             if(FileName == "DY.root"):
-                ZPTWeighting[0] = ZPTWeighting[0] * NominalWeight
-            if(Trigger24 or Trigger27):
-                ZPTWeighting[0] = ZPTWeighting[0] * SecondWorkSpace.function("m_trg24_27_kit_data").getVal()/SecondWorkSpace.function("m_trg24_27_kit_mc").getVal()
-            else:
-                ZPTWeighting[0] = ZPTWeighting[0] * (SecondWorkSpace.function("m_trg20_data").getVal()/SecondWorkSpace.function("m_trg20_mc").getVal())*TheTree.DiTauTriggerWeight
+                ZPTWeighting[0] = ZPTWeighting[0] * NominalWeight            
 
         #create the shape stuff
         ZPTWeighting_DOWN[0] = 0.9 * ZPTWeighting[0]
