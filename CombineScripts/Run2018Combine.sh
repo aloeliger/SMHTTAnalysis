3ea62b96eca6ec0038d13170a871a0046e6994c1
Andrew Loeliger <aloelige@cern.ch>
2019-05-02 19:19:57 +0200

    Updates to 2018 combine model and sample splitting

--- CombineScripts/Run2018Combine.sh
+++ CombineScripts/Run2018Combine.sh
@@ -1,40 +1,64 @@
 #!/usr/bin/bash
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/Data.root --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/Data.root
 
-#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY_T.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY_L.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY_T.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/DY_L.root --MakeDYShape --UseTES --UseJES --MakeZLShape --MakeRecoilUncertainties
 
-#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/Embedded.root --UseTES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/Embedded.root --UseTES 
 
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTTo2L2Nu.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToHadronic.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToSemiLeptonic.root  --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTTo2L2Nu.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTTo2L2Nu_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTTo2L2Nu_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToHadronic.root --UseTES --UseJES 
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToHadronic_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToHadronic_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToSemiLeptonic.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToSemiLeptonic_L.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/TTToSemiLeptonic_T.root  --UseTES --UseJES
 
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WW.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZZ.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WZ.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WW.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WW_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WW_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZZ.root --UseTES --UseJES 
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZZ_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZZ_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WZ.root --UseTES --UseJES 
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WZ_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WZ_T.root --UseTES --UseJES
 
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZLL.root  --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZNuNu.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZLL.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZLL_L.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZLL_T.root  --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZNuNu.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZNuNu_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/EWKZNuNu_T.root --UseTES --UseJES
 
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_top.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_antitop.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_top.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_antitop.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_top.root --UseTES --UseJES 
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_top_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_top_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_antitop.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_antitop_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_t_antitop_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_top.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_top_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_top_T.root --UseTES --UseJES
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_antitop.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_antitop_L.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ST_tW_antitop_T.root --UseTES --UseJES
 
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZH.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/VBF.root --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WHPlus.root  --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WHMinus.root  --UseTES --UseJES --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ggH.root --UseTES --UseJES --MakeggHTheoryShape --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ZH.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/VBF.root --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WHPlus.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/WHMinus.root  --UseTES --UseJES
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018/ggH.root --UseTES --UseJES --MakeggHTheoryShape
 
 #Create the rolled up fake shapes
-python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018AntiIso/Data.root --UseFakeFactor --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018AntiIso/Data.root --UseFakeFactor
 
 #Create the TTBarContamination shapes needed
-#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTTo2L2Nu.root --MakeTTbarContamination --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTToSemiLeptonic.root --MakeTTbarContamination --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
-#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTToHadronic.root --MakeTTbarContamination --numZeroJetBins 10 --numBoostedBins 10 --numVBFBins 10
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTTo2L2Nu.root --MakeTTbarContamination
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTToSemiLeptonic.root --MakeTTbarContamination
+#python GenerateRolledPlots.py 2018 ../SelectedNtuples/2018TTContamination/TTToHadronic.root --MakeTTbarContamination
 
 python SortRolledDistributions.py 2018
