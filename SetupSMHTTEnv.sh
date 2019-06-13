47b2de3265f25f3f13835b12b13969a3b3014463
Andrew Loeliger <aloelige@cern.ch>
2019-06-13 16:26:43 -0400

    Added in support for scripts and macros repo

--- SetupSMHTTEnv.sh
+++ SetupSMHTTEnv.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 export PYTHONPATH=$PYTHONPATH":"$CMSSW_BASE"/src/SMHTTAnalysis/NtuplePolishing/triggerSF/"
+export PYTHONPATH=$PYTHONPATH":"$CMSSW_BASE"/src/SMHTTAnalysis/ScriptsAndMacros/"
